13f0759dad0e20eaec0e6a46ead847fa13ac60d3
AleGon98 <ag435@lindenwood.edu>
2020-02-13 19:32:15 -0600

    Started adding functionality

--- src/app/components/App.vue
+++ src/app/components/App.vue
@@ -1,5 +1,57 @@
 <template>
-  <nav class="navbar navbar-light bg-light">
-    <a href="/" class="navbar-brand">MEVN Stack</a>
-  </nav>
+  <div>
+    <nav class="navbar navbar-light bg-light">
+      <a href="/" class="navbar-brand">MEVN Stack</a>
+    </nav>
+    <div class="container">
+      <div class="row pt-5">
+        <div class="col-md-5">
+          <div class="card">
+            <div class="card-body">
+              <form @submit.prevent="addTask">
+                <div class="form-group">
+                  <input
+                    type="text"
+                    v-model="task.title"
+                    placeholder="Insert a task"
+                    class="form-control"
+                  />
+                </div>
+                <div class="form-group">
+                  <textarea
+                    v-model="task.description"
+                    cols="30"
+                    rows="10"
+                    class="form-control"
+                    placeholder="Insert a description"
+                  ></textarea>
+                </div>
+                <button class="btn btn-primary btn-block">Send</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script lang='ts'>
+import Vue from "vue";
+
+export default Vue.extend({
+  data() {
+    return {
+      task: {
+        title: "",
+        description: ""
+      }
+    };
+  },
+  methods: {
+    addTask() {
+      console.log(this.task);
+    }
+  }
+});
+</script>
