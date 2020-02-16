6c9141af3539cad24bf2b2138132f77ac42c580d
AleGon98 <ag435@lindenwood.edu>
2020-02-15 22:31:31 -0600

    Finished application

--- src/app/components/App.vue
+++ src/app/components/App.vue
@@ -8,7 +8,7 @@
         <div class="col-md-5">
           <div class="card">
             <div class="card-body">
-              <form @submit.prevent="addTask">
+              <form @submit.prevent="sendTask">
                 <div class="form-group">
                   <input
                     type="text"
@@ -26,7 +26,12 @@
                     placeholder="Insert a description"
                   ></textarea>
                 </div>
-                <button class="btn btn-primary btn-block">Send</button>
+                <template v-if="edit === false">
+                  <button class="btn btn-primary btn-block">Send</button>
+                </template>
+                <template v-else>
+                  <button class="btn btn-primary btn-block">Update</button>
+                </template>
               </form>
             </div>
           </div>
@@ -43,6 +48,10 @@
               <tr v-for="task of tasks">
                 <td>{{ task.title }}</td>
                 <td>{{ task.description }}</td>
+                <td>
+                  <button @click="deleteTask(task._id)" class="btn btn-danger">Delete</button>
+                  <button @click="editTask(task._id)" class="btn btn-secondary">Edit</button>
+                </td>
               </tr>
             </tbody>
           </table>
@@ -64,26 +73,44 @@ export default {
   data() {
     return {
       task: new Task(),
-      tasks: []
+      tasks: [],
+      edit: false,
+      taskToEdit: ''
     };
   },
   created() {
     this.getTasks();
   },
   methods: {
-    addTask() {
-      fetch('/api/tasks', {
+    sendTask() {
+      if(this.edit === false) {
+        fetch('/api/tasks', {
         method: 'POST',
         body: JSON.stringify(this.task),
         headers: {
           'Accept': 'application/json',
           'Content-type': 'application/json'
         }
-      })
-      .then(res => res.json())
-      .then(data => {
-        this.getTasks()
-      })
+          })
+          .then(res => res.json())
+          .then(data => {
+            this.getTasks()
+          })
+      } else {
+        fetch('/api/tasks/' + this.taskToEdit, {
+          method: "PUT",
+          body: JSON.stringify(this.task),
+          headers: {
+          'Accept': 'application/json',
+          'Content-type': 'application/json'
+          }
+        })
+        .then(res => res.json())
+        .then(data => {
+          this.getTasks()
+          this.edit = false
+        })
+      }
 
       this.task = new Task();
     },
@@ -94,6 +121,29 @@ export default {
         this.tasks = data;
         console.log(this.tasks)
       })
+    },
+    deleteTask(id) {
+      fetch("/api/tasks/" + id, {
+        method: "DELETE",
+        headers: {
+          'Accept': 'application/json',
+          'Content-type': 'application/json'
+        }
+      })
+      .then(res => res.json)
+      .then(data => {
+        this.getTasks()
+      })      
+      
+    },
+    editTask(id) {
+      fetch("/api/tasks/" + id)
+      .then(res => res.json())
+      .then(data => {
+        this.task = new Task(data.title, data.description)
+        this.taskToEdit = data._id
+        this.edit = true
+      })      
     }
   }
 };
